2244d3a1800976ea7a6157a23cf61349ed002883
unknown <ajfoucaultmo@mail.usf.edu>
2023-02-17 12:55:28 -0800

    Section 17: Register User Front-End 75. Intro. 76. Register User - Setup. 77. Axios Intro. 78. Register User - Overview. 79. Register User - Complete. 80. Programmatically Navigate To Dashboard. 81. Persist User In Local Storage.

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react';
 import { Logo, FormRow, Alert } from '../components';
 import Wrapper from '../assets/wrappers/RegisterPage';
 import { useAppContext } from '../context/appContext';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 // global context and useNavigate later
 
 const initialState = {
@@ -14,10 +16,10 @@ const initialState = {
 // global state
 
 function Register() {
+  const navigate = useNavigate();
   const [values, setValues] = useState(initialState);
-
-  // global context and useNavigate later
-  const { isLoading, showAlert, displayAlert } = useAppContext();
+  const { user, isLoading, showAlert, displayAlert, registerUser } =
+    useAppContext();
 
   const toggleMember = () => {
     setValues({ ...values, isMember: !values.isMember });
@@ -34,8 +36,23 @@ function Register() {
       displayAlert();
       return;
     }
+    const currentUser = { name, email, password };
+    if (isMember) {
+      console.log('already a member');
+    } else {
+      registerUser(currentUser);
+    }
     console.log(values);
   };
+
+  useEffect(() => {
+    if (user) {
+      setTimeout(() => {
+        navigate('/');
+      }, 3000);
+    }
+  }, [user, navigate]);
+
   return (
     <Wrapper className='full-page'>
       <form className='form' onSubmit={onSubmit}>
@@ -65,7 +82,7 @@ function Register() {
           value={values.password}
           handleChange={handleChange}
         />
-        <button type='submit' className='btn btn-block'>
+        <button type='submit' className='btn btn-block' disabled={isLoading}>
           submit
         </button>
 
