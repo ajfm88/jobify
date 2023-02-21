ae00d79e1f8e7249b3f427e92f246d31faa7e652
unknown <ajfoucaultmo@mail.usf.edu>
2023-02-20 20:22:09 -0800

    Section 20: Nested Pages and Protected Route 89. Nested Pages - Intro. 90. Dashboard Pages - Setup. 91. Dashboard Pages - Structure. 92. Dashboard Pages - Complete. 93. Protected Route.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,29 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Dashboard, Landing, Error, Register } from './pages';
+import { Register, Landing, Error } from './pages';
+import {
+  AllJobs,
+  Profile,
+  SharedLayout,
+  Stats,
+  AddJob,
+} from './pages/dashboard';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Dashboard />} />
+        <Route
+          path='/'
+          element={
+            <ProtectedRoute>
+              <SharedLayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<Stats />} />
+          <Route path='all-jobs' element={<AllJobs />} />
+          <Route path='add-job' element={<AddJob />} />
+          <Route path='profile' element={<Profile />} />
+        </Route>
         <Route path='/register' element={<Register />} />
         <Route path='/landing' element={<Landing />} />
         <Route path='*' element={<Error />} />
--- client/src/pages/Dashboard.js
+++
@@ -1,7 +0,0 @@
-import { useEffect } from 'react';
-
-function Dashboard() {
-  return <div>Dashboard Page</div>;
-}
-
-export default Dashboard;
