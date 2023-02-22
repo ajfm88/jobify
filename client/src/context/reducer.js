5b84c692936642a251b3404ad817617171a6b4d0
unknown <ajfoucaultmo@mail.usf.edu>
2023-02-22 13:14:42 -0800

    Section 22: Navbar 097. Navbar Structure - Intro. 098. Navbar Structure - Complete. 099. Toggle Sidebar. 100. Toggle Dropdown. 101. Logout.

--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -1,16 +1,14 @@
 import {
   DISPLAY_ALERT,
   CLEAR_ALERT,
-  REGISTER_USER_BEGIN,
-  REGISTER_USER_SUCCESS,
-  REGISTER_USER_ERROR,
-  LOGIN_USER_BEGIN,
-  LOGIN_USER_SUCCESS,
-  LOGIN_USER_ERROR,
   SETUP_USER_BEGIN,
   SETUP_USER_SUCCESS,
   SETUP_USER_ERROR,
+  TOGGLE_SIDEBAR,
+  LOGOUT_USER,
 } from './actions';
+
+import { initialState } from './appContext';
 
 const reducer = (state, action) => {
   if (action.type === DISPLAY_ALERT) {
@@ -27,59 +25,6 @@ const reducer = (state, action) => {
       showAlert: false,
       alertType: '',
       alertText: '',
-    };
-  }
-  if (action.type === REGISTER_USER_BEGIN) {
-    return { ...state, isLoading: true };
-  }
-  if (action.type === REGISTER_USER_SUCCESS) {
-    return {
-      ...state,
-      user: action.payload.user,
-      token: action.payload.token,
-      userLocation: action.payload.location,
-      jobLocation: action.payload.location,
-      isLoading: false,
-      showAlert: true,
-      alertType: 'success',
-      alertText: 'User Created! Redirecting...',
-    };
-  }
-  if (action.type === REGISTER_USER_ERROR) {
-    return {
-      ...state,
-      isLoading: false,
-      showAlert: true,
-      alertType: 'danger',
-      alertText: action.payload.msg,
-    };
-  }
-  if (action.type === LOGIN_USER_BEGIN) {
-    return {
-      ...state,
-      isLoading: true,
-    };
-  }
-  if (action.type === LOGIN_USER_SUCCESS) {
-    return {
-      ...state,
-      isLoading: false,
-      user: action.payload.user,
-      token: action.payload.token,
-      userLocation: action.payload.location,
-      jobLocation: action.payload.location,
-      showAlert: true,
-      alertType: 'success',
-      alertText: 'Login Successful! Redirecting...',
-    };
-  }
-  if (action.type === LOGIN_USER_ERROR) {
-    return {
-      ...state,
-      isLoading: false,
-      showAlert: true,
-      alertType: 'danger',
-      alertText: action.payload.msg,
     };
   }
   if (action.type === SETUP_USER_BEGIN) {
@@ -105,6 +50,18 @@ const reducer = (state, action) => {
       showAlert: true,
       alertType: 'danger',
       alertText: action.payload.msg,
+    };
+  }
+  if (action.type === TOGGLE_SIDEBAR) {
+    return { ...state, showSidebar: !state.showSidebar };
+  }
+  if (action.type === LOGOUT_USER) {
+    return {
+      ...initialState,
+      user: null,
+      token: null,
+      userLocation: '',
+      jobLocation: '',
     };
   }
   throw new Error(`no such action :${action.type}`);
