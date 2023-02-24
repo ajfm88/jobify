49f859b8b1f3d3711e12a6a1383a563abec18110
unknown <ajfoucaultmo@mail.usf.edu>
2023-02-24 12:40:37 -0800

    Section 24: Big Sidebar 106. Nav Links Component. 107. Big Sidebar.

--- client/src/components/BigSidebar.js
+++ client/src/components/BigSidebar.js
@@ -1,11 +1,31 @@
-import Wrapper from '../assets/wrappers/BigSidebar';
+import Wrapper from '../assets/wrappers/SmallSidebar';
+import { FaTimes } from 'react-icons/fa';
+import { useAppContext } from '../context/appContext';
 
-const BigSidebar = () => {
+import Logo from './Logo';
+import NavLinks from './NavLinks';
+
+const SmallSidebar = () => {
+  const { showSidebar, toggleSidebar } = useAppContext();
   return (
     <Wrapper>
-      <h4>Big sidebar</h4>
+      <div
+        className={
+          showSidebar ? 'sidebar-container show-sidebar' : 'sidebar-container'
+        }
+      >
+        <div className='content'>
+          <button type='button' className='close-btn' onClick={toggleSidebar}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <NavLinks toggleSidebar={toggleSidebar} />
+        </div>
+      </div>
     </Wrapper>
   );
 };
 
-export default BigSidebar;
+export default SmallSidebar;
