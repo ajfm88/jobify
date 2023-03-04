722fce44c3fa4be266131b917de47bdfc28cb2fe
unknown <ajfoucaultmo@mail.usf.edu>
2023-03-04 08:41:49 -0800

    Section 35: Edit, Delete Job - Server 149. Edit Job Controller. 150. Alternative Approach. 151. Check Permissions - Intro. 152. Check Permissions - Complete. 153. Delete Job Controller.

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,6 +1,7 @@
 import Job from '../models/Job.js';
 import { StatusCodes } from 'http-status-codes';
 import { BadRequestError, NotFoundError } from '../errors/index.js';
+import checkPermissions from '../utils/checkPermissions.js';
 
 const createJob = async (req, res) => {
   const { position, company } = req.body;
@@ -22,10 +23,47 @@ const getAllJobs = async (req, res) => {
     .json({ jobs, totalJobs: jobs.length, numOfPages: 1 });
 };
 const updateJob = async (req, res) => {
-  res.send('update jobs');
+  const { id: jobId } = req.params;
+  const { company, position, status } = req.body;
+
+  if (!position || !company) {
+    throw new BadRequestError('Please provide all values');
+  }
+  const job = await Job.findOne({ _id: jobId });
+
+  if (!job) {
+    throw new NotFoundError(`No job with id :${jobId}`);
+  }
+
+  // check permissions
+  // req.user.userId (string) === job.createdBy(object)
+  // throw new UnAuthenticatedError('Not authorized to access this route')
+
+  // console.log(typeof req.user.userId)
+  // console.log(typeof job.createdBy)
+
+  checkPermissions(req.user, job.createdBy);
+
+  const updatedJob = await Job.findOneAndUpdate({ _id: jobId }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(StatusCodes.OK).json({ updatedJob });
 };
 const deleteJob = async (req, res) => {
-  res.send('delete jobs');
+  const { id: jobId } = req.params;
+
+  const job = await Job.findOne({ _id: jobId });
+
+  if (!job) {
+    throw new NotFoundError(`No job with id : ${jobId}`);
+  }
+
+  checkPermissions(req.user, job.createdBy);
+
+  await job.remove();
+  res.status(StatusCodes.OK).json({ msg: 'Success! Job removed' });
 };
 const showStats = async (req, res) => {
   res.send('show stats');
