2b6fc16857e85fd75af3416913ebae18b8a941f2
unknown <ajfoucaultmo@mail.usf.edu>
2023-03-05 09:56:55 -0800

    Section 39: Stats - Front-End 162. Fetch Stats. 163. Stats Page Structure. 164. Stats Container. 165. Stats Item.

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -24,6 +24,8 @@ import {
   EDIT_JOB_BEGIN,
   EDIT_JOB_SUCCESS,
   EDIT_JOB_ERROR,
+  SHOW_STATS_BEGIN,
+  SHOW_STATS_SUCCESS,
 } from './actions';
 
 // set as default
@@ -53,6 +55,8 @@ const initialState = {
   totalJobs: 0,
   numOfPages: 1,
   page: 1,
+  stats: {},
+  monthlyApplications: [],
 };
 
 const AppContext = React.createContext();
@@ -275,7 +279,24 @@ const AppProvider = ({ children }) => {
       logoutUser();
     }
   };
+  const showStats = async () => {
+    dispatch({ type: SHOW_STATS_BEGIN });
+    try {
+      const { data } = await authFetch('/jobs/stats');
+      dispatch({
+        type: SHOW_STATS_SUCCESS,
+        payload: {
+          stats: data.defaultStats,
+          monthlyApplications: data.monthlyApplications,
+        },
+      });
+    } catch (error) {
+      console.log(error.response);
+      // logoutUser()
+    }
 
+    clearAlert();
+  };
   return (
     <AppContext.Provider
       value={{
@@ -290,6 +311,7 @@ const AppProvider = ({ children }) => {
         setEditJob,
         deleteJob,
         editJob,
+        showStats,
       }}
     >
       {children}
