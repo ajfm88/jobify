0ddeea8ea414f27f916b1fa1a3af2a1eee39eac4
unknown <ajfoucaultmo@mail.usf.edu>
2023-03-08 06:47:56 -0800

    Section 44: Pagination - Server 184. Pagination - Intro. 185. Page and Limit. 186. Total Jobs and Number of Pages.

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -4,6 +4,7 @@ import { BadRequestError, NotFoundError } from '../errors/index.js';
 import checkPermissions from '../utils/checkPermissions.js';
 import mongoose from 'mongoose';
 import moment from 'moment';
+
 const createJob = async (req, res) => {
   const { position, company } = req.body;
 
@@ -16,26 +17,23 @@ const createJob = async (req, res) => {
   const job = await Job.create(req.body);
   res.status(StatusCodes.CREATED).json({ job });
 };
+
 const getAllJobs = async (req, res) => {
   const { search, status, jobType, sort } = req.query;
-
   const queryObject = {
     createdBy: req.user.userId,
   };
-
+  if (search) {
+    queryObject.position = { $regex: search, $options: 'i' };
+  }
   if (status !== 'all') {
     queryObject.status = status;
   }
   if (jobType !== 'all') {
     queryObject.jobType = jobType;
   }
-  if (search) {
-    queryObject.position = { $regex: search, $options: 'i' };
-  }
-  // NO AWAIT
   let result = Job.find(queryObject);
 
-  // chain sort conditions
   if (sort === 'latest') {
     result = result.sort('-createdAt');
   }
@@ -48,12 +46,22 @@ const getAllJobs = async (req, res) => {
   if (sort === 'z-a') {
     result = result.sort('-position');
   }
+
+  // setup pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
   const jobs = await result;
 
-  res
-    .status(StatusCodes.OK)
-    .json({ jobs, totalJobs: jobs.length, numOfPages: 1 });
+  const totalJobs = await Job.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalJobs / limit);
+
+  res.status(StatusCodes.OK).json({ jobs, totalJobs, numOfPages });
 };
+
 const updateJob = async (req, res) => {
   const { id: jobId } = req.params;
   const { company, position, status } = req.body;
@@ -83,6 +91,7 @@ const updateJob = async (req, res) => {
 
   res.status(StatusCodes.OK).json({ updatedJob });
 };
+
 const deleteJob = async (req, res) => {
   const { id: jobId } = req.params;
 
@@ -97,6 +106,7 @@ const deleteJob = async (req, res) => {
   await job.remove();
   res.status(StatusCodes.OK).json({ msg: 'Success! Job removed' });
 };
+
 const showStats = async (req, res) => {
   let stats = await Job.aggregate([
     { $match: { createdBy: mongoose.Types.ObjectId(req.user.userId) } },
