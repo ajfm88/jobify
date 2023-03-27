ee7d8e44f6b14020244c786c69396a7b99189278
Alejandro J. Foucault <ajfoucaultmo@mail.usf.edu>
2023-03-26 17:15:08 -0700

    Update landing page hero text.

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -22,12 +22,15 @@ const Landing = () => {
               job <span>tracking</span> app
             </h1>
             <p>
-              Find Your Next Career Opportunity with Jobify! Introducing the
-              ultimate Job Application Tracking Website. Keep track of all your
+              Find your next career opportunity with Jobify! Introducing the
+              ultimate job application tracking website. Keep track of all your
               job applications in one place, receive real-time updates, and
               never miss an opportunity. Say goodbye to scattered job
               applications and never-ending email threads. Start your job search
-              journey with confidence today and land your dream job.
+              journey with confidence today and land your dream job.<br />
+              There is also a demo account available for testing purposes. 
+              In the login screen, just click on the "Demo App" button to test
+              the application using that demo account.
             </p>
             <Link to='/register' className='btn btn-hero'>
               Login/Register
